849c834fee931e534e5202a3b4fa2774f55136a5
Mart <smartha55@hotmail.com>
2018-12-19 21:59:30 +0700

    add paging

---
+++ src/components/Table.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table'
+import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css'
+
+class Tables extends Component {
+  render() {
+    console.log(this.props)
+    const {data} = this.props
+    // const data = this.props.location.state.options;
+    var productList = data.map( e=>{
+      return(
+        <Tr>
+          <Td>{e.productID}</Td>
+          <Td>{e.brand}</Td>
+          <Td>{e.model}</Td>
+          <Td>{e.details[0]}</Td>
+          <Td>{e.details[1]}</Td>
+          <img 
+            style={{
+            maxWidth:'230px',
+            maxHeight:'95px',
+            width: null,
+            height: null,
+            marginBottom:10
+          }} 
+            src={e.pictures[0]}
+          />
+          <Td>{e.quantity}</Td>
+        </Tr>
+      )
+    })
+    return (
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>Product Id</Th>
+            <Th>Brand</Th>
+            <Th>Model</Th>
+            <Th>detail one</Th>
+            <Th>detail two</Th>
+            <Th>Photo</Th>
+            <Th>Quantity</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {productList}
+        </Tbody>
+      </Table>
+
+    )
+  }
+}
+
+export default Tables;
+
--- src/components/pages/TablePage.js
+++ src/components/pages/TablePage.js
@@ -1,51 +1,72 @@
 import React, { Component } from 'react'
-import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table'
 import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css'
+import Table from '../Table';
+import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
+
 
 class TablePage extends Component {
-  render() {
-    console.log(this.props.location.state.options)
-    const data = this.props.location.state.options;
-    var productList = data.map( e=>{
-      return(
-        <Tr>
-          <Td>{e.productID}</Td>
-          <Td>{e.brand}</Td>
-          <Td>{e.model}</Td>
-          <Td>{e.details[0]}</Td>
-          <Td>{e.details[1]}</Td>
-          <img 
-            style={{
-            maxWidth:'230px',
-            maxHeight:'95px',
-            width: 'auto',
-            height: 'auto',
-            marginBottom:10
-          }} 
-            src={e.pictures[0]}
-          />
-          <Td>{e.quantity}</Td>
-        </Tr>
-      )
-    })
-    return (
-      <Table>
-        <Thead>
-          <Tr>
-            <Th>Product Id</Th>
-            <Th>Brand</Th>
-            <Th>Model</Th>
-            <Th>detail one</Th>
-            <Th>detail two</Th>
-            <Th>Photo</Th>
-            <Th>Quantity</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {productList}
-        </Tbody>
-      </Table>
 
+  state = {
+    currentPage: 1,
+    productPerPage: 5
+  };
+
+
+
+  handleClick = (event) => {
+    console.log(event.target.id)
+    this.setState({
+      currentPage: event.target.id
+    })
+  }
+
+  render() {
+    const { currentPage, productPerPage } = this.state;
+    const data = this.props.location.state.options;
+
+    // Logic for displaying current todos
+    const indexOfLastTodo = currentPage * productPerPage;
+    const indexOfFirstTodo = indexOfLastTodo - productPerPage;
+    const currentProducts = data.slice(indexOfFirstTodo, indexOfLastTodo);
+
+    // Logic for displaying page numbers
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(data.length / productPerPage); i++) {
+      pageNumbers.push(i);
+    }
+
+    const renderPageNumbers = pageNumbers.map(number => {
+      return (
+        <PaginationItem>
+          <PaginationLink
+            key={number}
+            id={number}
+            onClick={this.handleClick}
+          >
+            {number}
+          </PaginationLink>
+        </PaginationItem>
+      );
+    });
+
+    return (
+      <div>
+        <Table data={currentProducts} />
+        <Pagination aria-label="Page navigation example" 
+        //   style={{
+        //     position: 'absolute',
+        //     right: '100px'
+        // }}
+        >
+          <PaginationItem >
+            <PaginationLink previous onClick={() => this.setState({ currentPage: this.state.currentPage - 1 != 0 ? this.state.currentPage - 1 : 1 })} />
+          </PaginationItem>
+          {renderPageNumbers}
+          <PaginationItem>
+            <PaginationLink next onClick={() => this.setState({ currentPage: this.state.currentPage + 1 <= pageNumbers.length ? this.state.currentPage + 1 : this.state.currentPage })} />
+          </PaginationItem>
+        </Pagination>
+      </div>
     )
   }
 }
